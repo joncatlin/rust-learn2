d99a34babcbea334f4fc933518ad5a6a49135cf4
Jon Catlin <jonc@destini.com>
2019-11-28 03:19:06 -0700

    Completed ownership exercise

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -12,6 +12,11 @@ fn main() {
     calculate_length(&s);
 
     change_s(&mut s);
+
+    println!("First word found at inx={}", first_word(&s));
+
+    println!("First word found is: {}", first_word_slice(&s));
+    
 }
 
 /* The following code generates a move compile time error because s1 was moved to s2 and cannot then be refereced.
@@ -44,4 +49,31 @@ fn change_s (s: &mut String) -> usize {
     let s_len = s.len();
     println!("s.len={} s={}",s_len, s);
     s_len
+}
+
+// Function that returns the index of the first space found in a string
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+// Function that returns the first word found in a string as a slice. The
+// function signature takes a slice and returns a slice.
+fn first_word_slice(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
